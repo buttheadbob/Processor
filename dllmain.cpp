572ce7f0ccf10a8fcaa3328931173715ebfffa61
SentorX <evilowl2010@gmail.com>
2023-04-29 21:06:29 +0200

    Add project files.

---
+++ dllmain.cpp
@@ -0,0 +1,3 @@
+#pragma comment(lib, "UnmanagedHelper.lib")
+#include "C:/RiderProjects/UnmanagedHelper/UnmanagedHelper.h" // Adjust the relative path accordingly
+std::vector<UnmanagedLogicalProcessorInfo> logicalProcessors = GetUnmanagedLogicalProcessors();
